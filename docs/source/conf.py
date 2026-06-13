195a787a9976145cc2dd75813739ef1b27e728f7
Rohit Goswami <rgoswami@ieee.org>
2026-06-13 04:47:55 -0500

    docs(site): load antics tracker

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 html_theme = "shibuya"
 html_static_path = ["_static"]
+html_js_files = [
+    ("https://antics-api.turtletech.us/antics.js", {"defer": "defer"}),
+]
 
 html_context = {
     "source_type": "github",
